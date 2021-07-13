0d771a94d4bc3db1e068fb9e72662a5e8f911909
onlycan17 <onlycan17@gmail.com>
2021-07-13 21:00:31 +0900

    update

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,11 @@
-import { getMovie } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
-    movies: (_, {limit, rating}) => getMovie(limit,rating)
-  },
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
+  }
 };
 
 export default resolvers;
